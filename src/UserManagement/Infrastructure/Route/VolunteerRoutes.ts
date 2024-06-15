cffaf8dcb0b9d7e74282348790b6bb4040c191f4
KarineAlsa <saremana@hotmail.com>
2024-06-15 01:27:53 -0600

    add-temporal[USM]:Get volunteer

--- src/UserManagement/Infrastructure/Route/VolunteerRoutes.ts
+++ src/UserManagement/Infrastructure/Route/VolunteerRoutes.ts
@@ -3,5 +3,9 @@ import {   registerVolunteerController } from "../Dependencies";
 const volunteerRouter = express.Router();
 
 volunteerRouter.post("/",registerVolunteerController.run.bind(registerVolunteerController));
+volunteerRouter.get("/", (req, res) => {
+    res.send('Hola mundo');
+});
+
 
 export default volunteerRouter;
