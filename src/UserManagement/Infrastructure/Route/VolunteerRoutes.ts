57d9a9fd1488c958da8f743472e27eb36a58acef
KarineAlsa <saremana@hotmail.com>
2024-06-15 03:27:53 -0600

    add-temporal[USM]:Get volunteer

--- src/UserManagement/Infrastructure/Route/VolunteerRoutes.ts
+++ src/UserManagement/Infrastructure/Route/VolunteerRoutes.ts
@@ -1,8 +1,8 @@
 import  express  from "express";
 import {   registerVolunteerController } from "../Dependencies";
-import dotenv from 'dotenv'
+
 const volunteerRouter = express.Router();
-dotenv.config()
+
 volunteerRouter.post("/",registerVolunteerController.run.bind(registerVolunteerController));
 volunteerRouter.get("/", (req, res) => {
     res.send(`Hello World ${process.env.HOST}`);
